33129411903ef7a37b7c526f42d75d601be84cb9
Aronne Brivio <aroblu94@gmail.com>
2016-03-31 21:09:08 +0200

    Added number(category, childDes list) OK
    Refactoring for childDes -> Fixed #3

--- 8/esami.fsx
+++ 8/esami.fsx
@@ -108,6 +108,34 @@ type category =
 
 // i)
 type names = string;;
-type childDes = category;;
+type child = {name : names; category : category};;
+type childDes = Child of names * category;;
+// ii)
+let rec number (cat, cd) =
+    match cd with
+    | [] -> 0
+    | [x] -> 
+        match x with
+        | (_,c) when c=cat -> 1
+        | (_,_) -> 0
+    | x::xs ->
+        let n = number (cat, xs)
+        match x with
+        | (_,c) when c=cat -> 1+n
+        | (_,_) -> n
 
-
+    // test
+    let ch1 = {name="Rossi"; category=Daycare};;
+    let ch2 = {name="Bianchi"; category=Nursery};;
+    let ch3 = {name="Brivio"; category=Recreation};;
+    let ch4 = {name="Pignatale"; category=Recreation};;
+    let ch5 = {name="Fortunato"; category=Daycare};;
+    let ch6 = {name="Lupoli"; category=Daycare};;
+    let cd1 = ("Rossi", Daycare);;
+    let cd2 = ("Bianchi", Nursery);;
+    let cd3 = ("Brivio", Recreation);;
+    let cd4 = ("Pignatale", Recreation);;
+    let cd5 = ("Fortunato", Daycare);;
+    let cd6 = ("Lupoli", Daycare);;
+    let cdl = [cd1; cd2; cd3; cd4; cd5; cd6];;
+    number(Recreation, cdl);;
