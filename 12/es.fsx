4e381acaef5857841bf9604601fff9c621fe59dc
Aronne Brivio <aroblu94@gmail.com>
2016-04-18 21:48:11 +0200

    Added comparison between recursive and iterative

--- 12/es.fsx
+++ 12/es.fsx
@@ -21,3 +21,36 @@ let isuml ls =
 // test
 isuml [1..10];; // 55
 
+
+// CONFRONTO CON VERSIONE RICORSIVA
+let rec suml = function
+    | [] -> 0
+    | x :: xs -> x + suml xs;;
+
+(*
+    Nell'interprete dare il comando 
+
+     #time
+
+    che, dopo ogni computazione, stampa alcuni dati sulle risorse utilizzate 
+    (tempo CPU, uso garbage collector, ecc.)
+
+    Provare ad eseguire delle chiamate della forma
+
+      suml [ 1 ..K ]      
+     isuml [ 1 .. K ]
+
+    con K intero grande a piacere.
+    Tenere presente che le liste sono costruite nello heap.
+*)
+#time
+suml [1..10000000];;
+#time
+#time
+isuml [1..10000000];;
+#time
+
+(*
+---> versione iterativa: Real: 00:00:04.341, CPU: 00:00:04.500, GC gen0: 71, gen1: 6
+---> versione ricorsiva: Stack Overflow
+*)
