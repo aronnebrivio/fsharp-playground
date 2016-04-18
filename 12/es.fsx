45c61c329324ad4771468de3098d023d3c9477b8
Aronne Brivio <aroblu94@gmail.com>
2016-04-18 21:35:30 +0200

    Added isuml ls

--- 12/es.fsx
+++ 12/es.fsx
@@ -11,3 +11,13 @@
 
 	in cui nel secondo parametro viene accumulata la somma degli elementi.
 *)
+let isuml ls =
+    let rec sumlA (ls,acc) =
+        match ls with
+        | [] -> acc
+        | x::xs -> sumlA (xs, acc+x)
+    sumlA(ls,0);;
+
+// test
+isuml [1..10];; // 55
+
