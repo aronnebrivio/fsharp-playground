2a4fd2e49e08e6f1d212eb4b725c6276782a3a6f
Aronne Brivio <aroblu94@gmail.com>
2016-04-18 21:31:03 +0200

    Added es.fsx

---
+++ 12/es.fsx
@@ -0,0 +1,13 @@
+(*
+	Definire la funzione iterativa
+
+	    isuml :  int list -> int
+
+	che calcola la somma degli elementi di una lista di interi.
+
+	Usare la funzione ausiliaria  
+
+	     sumlA : int list * int   -> int
+
+	in cui nel secondo parametro viene accumulata la somma degli elementi.
+*)
