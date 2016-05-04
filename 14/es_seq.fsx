f3239e7bd946a965b16b5f43e982b3e9110d1d83
Aronne Brivio <aroblu94@gmail.com>
2016-05-04 11:01:32 +0200

    added es_seq.fsx

--- 14/es_seq.fsx
+++ 14/es_seq.fsx
@@ -1,3 +1,19 @@
+let sq1 = seq {
+  yield 0  // yield genera un elemento
+  yield 1
+  yield 2
+  yield 3
+  } ;;
+
+// sq1 : seq<int> definisce la sequenza seq [ 0; 1; 2; 3 ]
+
+let sq2 = seq{
+  yield 100
+  yield! sq1  // yield! aggiunge tutti gli elementi di sq1
+  yield 200
+  yield! sq1
+  } ;;
+
 (*
 i) Usando delle opportune sequence expression, definire la funzioni
 
@@ -22,3 +38,39 @@ let append sq1 sq2 = seq {
     yield! sq2
 };;
 
+let head sq = Seq.item 0 sq;;
+
+let tail sq = Seq.skip 1 sq;;
+
+(*
+    Usando  Seq.initInfinite definire le seguenti sequenze infinite:
+
+    - nat : sequenza dei numeri naturali 0, 1, 2, ...
+    - nat1: sequenza dei numeri naturali senza il numero 5
+    - nat2: sequenza dei numeri naturali in cui il numero 5 e' sostituito da -5
+    - even10 : sequenza dei numeri pari n >= 10  
+    - sqTrue : sequenza costante true, true, true, ....
+    - sqTrueFalse: sequenza true, false, true, false, true, false, ...
+
+    Per ciascuna sequenza generare la lista dei primi 10 elementi.
+*)
+let nat = Seq.initInfinite (fun x -> x);;
+
+let nat1 = seq {
+    yield! [0..4]
+    yield! Seq.skip 6 nat
+};;
+
+let nat2 = seq {
+    yield! [0..4]
+    yield -5
+    yield! Seq.skip 6 nat
+};;
+
+let even10 = seq {
+    yield! (Seq.skip 5 (Seq.initInfinite (fun x -> x*2)))
+} ;;
+
+let sqTrue = Seq.initInfinite (fun x -> true);;
+
+let sqTrueFalse = Seq.initInfinite (fun x -> if x%2=0 then true else false);;
