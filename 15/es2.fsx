2d77cb2692504af28c31bec344dc0e953de0d8d3
Aronne Brivio <aroblu94@gmail.com>
2016-05-05 13:27:33 +0200

    Eratostene ii) done

--- 15/es2.fsx
+++ 15/es2.fsx
@@ -40,3 +40,33 @@ let sift i sq = Seq.filter (fun x -> x%i>0) sq;;
 //test
 sift 2 nat |> Seq.take 15 |> Seq.toList;;
 sift 3 nat |> Seq.take 15 |> Seq.toList;;
+
+(*
+ii) Definire la funzione ricorsiva
+
+    sieve : seq<int> -> seq<int>
+
+che esegue   il crivello di Eratostene su una sequenza infinita di interi.
+Piu' precisamente, data una sequenza sq di interi, va eseguito il seguente processo infinito:
+
+-----------------------------------------------------------------------------------------
+| 1. x0  :=  primo elemento di sq                                                        |  
+| 2. Cancella da sq i multipli propri di x0                                              | 
+|    (ossia, x0 rimane in sq e sono cancellati da sq i multipli di x0 maggiori di x0)    |
+| 3. Ripeti il processo da 1                                                             |
+----------------------------------------------------------------------------------------- 
+
+Al passo 2 usare la funzione sift.
+
+*)
+let rec sieve sq =
+    seq {
+        let first = Seq.item 0 sq
+        if first=0 || first=1 then
+            yield! sieve (Seq.skip 1 sq)
+        else
+            yield first
+            yield! sieve (sift first sq)
+    };;
+//test
+sieve nat;;
